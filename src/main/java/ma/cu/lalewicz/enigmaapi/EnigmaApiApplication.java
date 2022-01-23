f64451cb8e9dff1ba8fb918b55b0dcee422755fa
Tomasz Lalewicz <lalewicz.tomasz@gmail.com>
2022-01-23 23:34:44 +0100

    first commit

---
+++ src/main/java/ma/cu/lalewicz/enigmaapi/EnigmaApiApplication.java
@@ -0,0 +1,13 @@
+package ma.cu.lalewicz.enigmaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnigmaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnigmaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ma/cu/lalewicz/enigmaapi/EnigmaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.cu.lalewicz.enigmaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnigmaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
